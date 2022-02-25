b598c9bb7ae2909a239705c54f00e70f6cc22801
cong <458366075@qq.com>
2022-02-25 12:59:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import '/assets/styles.css';
-
+import '~/assets/styles.css'
 import DefaultLayout from '~/layouts/Default.vue'
 import Header from '~/components/Header.vue'
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,17 @@
 const axios = require('axios')
 
-module.exports = axios.create({
+const request = axios.create({
   baseURL: `${process.env.GRIDSOME_API_URL}/api/`,
 });
+
+// request.interceptors.request.use(res => {
+//   console.log(res, 'start');
+//   return res
+// }, res => console.log(res, 'error'))
+
+// request.interceptors.response.use(res => {
+//   console.log(res);
+//   return res
+// }, res => console.log(res))
+
+module.exports = request
